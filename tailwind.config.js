0a53e369090d86e72d173d18b952251a1ba0e883
Lane G Smith <lane.smith.contact@gmail.com>
2023-08-05 18:33:05 -0700

    Create initial template

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./styles/*.{html,js,css}", "./pages/**/*.{html,js,css}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+}
+
