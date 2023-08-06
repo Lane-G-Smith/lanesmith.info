3f6db2d0963eee13d121c9ee7f5dc3b4754c2ce1
Lane G Smith <lane.smith.contact@gmail.com>
2023-08-05 18:46:57 -0700

    update Taliwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./styles/*.{html,js,css}", "./pages/**/*.{html,js,css}"],
+  content: ["./styles/*.{html,js,css}", "./pages/**/*.{html,js,css}", "./*.html"],
   darkMode: "class",
   theme: {
     extend: {},
